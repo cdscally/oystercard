989fab6fc7fba7529e6dbe2d5701a242c08ef6a4
Colin Scally <colin.scally@gmail.com>
2017-05-15 15:16:07 +0100

    Step 4, add test that balance should initialize as an integer, add initialize method with @balance instance variable set to '0'

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,8 @@ describe Oystercard do
   it 'has a balance' do
     expect(subject).to respond_to(:balance)
   end
+
+  it 'should initialize with a balance' do
+    expect(subject.balance.class).to eq Integer
+  end
 end
