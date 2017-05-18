62d5c4f43e5cb18ca0352b88720151c363d457d1
bannastre <bannastre@gmail.com>
2017-05-18 17:55:36 +0100

    Create #start, #finish, #get_journey_start, #get_journey_finish

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,9 +1,29 @@
+require_relative 'journey'
+
 class JourneyLog
   attr_reader :journeys, :journey_class
 
   def initialize(journey_class)
     @journey_class = journey_class
     @journeys = []
+  end
+
+  def start(station)
+    @journey = Journey.new(station)
+    get_journey_start
+  end
+
+  def finish(station)
+    @journey.complete_journey(station)
+    get_journey_finish
+  end
+
+  def get_journey_start
+    @journey.entry_station
+  end
+
+  def get_journey_finish
+    @journey.completed_journey[:exit]
   end
 
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -7,4 +7,15 @@ describe JourneyLog do
   it 'knows a log of journeys' do
     expect(journey_log.journeys).to eq []
   end
+
+  it 'should be able to start a new journey with a new entry station' do
+    journey_log.start(:entry_station)
+    expect(journey_log.get_journey_start).to eq :entry_station
+  end
+
+  it 'should be able to finish a new journey with a new entry station' do
+    journey_log.start(:entry_station)
+    journey_log.finish(:exit_station)
+    expect(journey_log.get_journey_finish).to eq :exit_station
+  end
 end
